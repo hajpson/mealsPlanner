9ff9ce07b118e9c3751200b7445d60918087be94
hajpson <kacpah@hotmail.com>
2024-05-27 17:45:49 +0200

    expo 51 upgrade, readme

--- App.tsx
+++ App.tsx
@@ -8,10 +8,10 @@ import { useCallback } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { EatingGoalProvider } from "./src/context/EatingGoalContext";
-import { MMKVProvider } from "./src/context/MMKVContext";
 import { MainNavigationContainer } from "./src/navigation/MainNavigationContainer";
 import { LoginScreen } from "./src/screens/LoginScreen";
 import { FLAGS } from "./src/utils/constants";
+import { MMKVProvider } from "./src/context/mmkvContext";
 
 const isInLoginMode = FLAGS.IS_USING_LOGIN_SCREEN;
 
